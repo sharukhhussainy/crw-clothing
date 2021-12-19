49c0352cb7f7812a05ae75f77160a8af8ec50923
sharukhhussainy <syedsharukhhussainy@gmail.com>
2021-12-19 18:36:37 +0530

    Created new components, finished styling of each component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import HomePage from "./components/home-page/home-page.component";
+import HomePage from "./pages/home-page/home-page.component";
 
 function App() {
   return (
